aa7a170f0f9d29bef61f026747d33c8539ba4ff5
cipepser <respepic@gmail.com>
2019-01-10 07:34:08 +0900

    use let expression

--- chap3/BmiTell.hs
+++ chap3/BmiTell.hs
@@ -10,5 +10,4 @@ bmiTell weight height
           fat = 30.0
 
 calcBmis :: [(Double, Double)] -> [Double]
-calcBmis xs = [bmi w h | (w, h) <- xs]
-    where bmi weight height = weight / height^2
+calcBmis xs = [bmi | (w, h) <- xs, let bmi = w / h ^2, bmi > 25.0]
