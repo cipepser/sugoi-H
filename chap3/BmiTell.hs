d2ac81a78f8aac627cdb46a0acfda1b0e1128921
cipepser <respepic@gmail.com>
2019-01-09 22:00:39 +0900

    name bmi values

--- chap3/BmiTell.hs
+++ chap3/BmiTell.hs
@@ -1,7 +1,10 @@
 bmiTell :: Double -> Double -> String
 bmiTell weight height
-    | bmi <= 18.5 = "You're underweight, you emo, you!"
-    | bmi <= 25.0 = "You're supporsedly normal. Pffft, Ibet you're ugly!"
-    | bmi <= 30.0 = "You're fat! Lose some weight, fatty!"
+    | bmi <= skinny = "You're underweight, you emo, you!"
+    | bmi <= normal = "You're supporsedly normal. Pffft, Ibet you're ugly!"
+    | bmi <= fat = "You're fat! Lose some weight, fatty!"
     | otherwise                 = "You're a whale, congratulations!"
     where bmi = weight / height^2
+          skinny = 18.5
+          normal = 25.0
+          fat = 30.0
