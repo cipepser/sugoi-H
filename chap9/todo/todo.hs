15f30c2923f7dda2495c55fb6e71074c8cca9f56
cipepser <respepic@gmail.com>
2019-05-01 12:18:32 +0900

    implement add

---
+++ chap9/todo/todo.hs
@@ -0,0 +1,16 @@
+import System.Environment
+import System.Directory
+import System.IO
+import Data.List
+
+dispatch :: String -> [String] -> IO ()
+dispatch "add" = add
+-- dispatch "view" = view
+-- dispatch "remove" = remove
+
+add :: [String] -> IO ()
+add [fileName, todoItem] = appendFile fileName (todoItem ++ "\n")
+
+main = do
+    (command:argList) <- getArgs
+    dispatch command argList
