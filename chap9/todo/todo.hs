7547da1fd5b3044169e02b04bd96cb44869457a5
cipepser <respepic@gmail.com>
2019-05-01 12:26:58 +0900

    implement remove for todo

--- chap9/todo/todo.hs
+++ chap9/todo/todo.hs
@@ -1,12 +1,13 @@
 import System.Environment
 import System.Directory
 import System.IO
+import Control.Exception
 import Data.List
 
 dispatch :: String -> [String] -> IO ()
 dispatch "add" = add
 dispatch "view" = view
--- dispatch "remove" = remove
+dispatch "remove" = remove
 
 add :: [String] -> IO ()
 add [fileName, todoItem] = appendFile fileName (todoItem ++ "\n")
@@ -17,6 +18,26 @@ view [fileName] = do
     let todoTasks = lines contents
         numberedTasks = zipWith (\n line -> show n ++ " - " ++ line) [0..] todoTasks
     putStr $ unlines numberedTasks
+
+remove :: [String] -> IO ()
+remove [fileName, numberString] = do
+    contents <- readFile fileName
+    let todoTasks = lines contents
+        numberedTasks = zipWith (\n line -> show n ++ " - " ++ line) [0..] todoTasks
+    putStrLn "These are your TODO-items:"
+    mapM_ putStrLn numberedTasks
+    let number = read numberString
+        newTodoItems = unlines $ delete (todoTasks !! number) todoTasks
+    bracketOnError (openTempFile "." "temp")
+        (\(tempName, tempHandle) -> do
+            hClose tempHandle
+            removeFile tempName)
+        (\(tempName, tempHandle) -> do
+            hPutStr tempHandle newTodoItems
+            hClose tempHandle
+            removeFile fileName
+            renameFile tempName fileName)
+
 
 main = do
     (command:argList) <- getArgs
