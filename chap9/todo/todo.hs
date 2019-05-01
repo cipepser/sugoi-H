e3f6a9a985c5a03bb2bf37c0404e3b71f3e166fd
cipepser <respepic@gmail.com>
2019-05-01 12:19:06 +0900

    implement view for todo

--- chap9/todo/todo.hs
+++ chap9/todo/todo.hs
@@ -5,11 +5,18 @@ import Data.List
 
 dispatch :: String -> [String] -> IO ()
 dispatch "add" = add
--- dispatch "view" = view
+dispatch "view" = view
 -- dispatch "remove" = remove
 
 add :: [String] -> IO ()
 add [fileName, todoItem] = appendFile fileName (todoItem ++ "\n")
+
+view :: [String] -> IO ()
+view [fileName] = do
+    contents <- readFile fileName
+    let todoTasks = lines contents
+        numberedTasks = zipWith (\n line -> show n ++ " - " ++ line) [0..] todoTasks
+    putStr $ unlines numberedTasks
 
 main = do
     (command:argList) <- getArgs
