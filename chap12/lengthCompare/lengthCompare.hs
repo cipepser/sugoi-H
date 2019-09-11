16e3db952c690cf7d9b7e5e979aed8b5aeb05872
cipepser <respepic@gmail.com>
2019-09-11 22:53:39 +0900

    add lengthCompare

---
+++ chap12/lengthCompare/lengthCompare.hs
@@ -0,0 +1,6 @@
+import Data.Monoid
+
+lengthCompare :: String -> String -> Ordering
+lengthCompare x y = let a = length x `compare` length y
+                        b = x `compare` y
+                    in if a == EQ then b else a
