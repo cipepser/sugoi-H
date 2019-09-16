e1e9488b06110c71c54b1ea0d3cf89def86cd7b9
cipepser <respepic@gmail.com>
2019-09-17 00:57:46 +0900

    update routine

--- chap13/tsuna/tsuna.hs
+++ chap13/tsuna/tsuna.hs
@@ -15,3 +15,11 @@ x -: f = f x
 
 banana :: Pole -> Maybe Pole
 banana _ = Nothing
+
+routine :: Maybe Pole
+routine = do
+    start  <- return (0, 0)
+    first  <- landLeft 2 start
+    -- Nothing
+    second <- landRight 2 first
+    landLeft 1 second
