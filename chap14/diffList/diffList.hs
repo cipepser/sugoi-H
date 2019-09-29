e698a9770840089a6041663d189f1a704da027f8
cipepser <respepic@gmail.com>
2019-09-29 13:22:56 +0900

    add diffList

---
+++ chap14/diffList/diffList.hs
@@ -0,0 +1,17 @@
+import Data.Monoid
+import Data.Semigroup
+
+newtype DiffList a = DiffList { getDiffList :: [a] -> [a] }
+
+toDiffList :: [a] -> DiffList a
+toDiffList xs = DiffList (xs++)
+
+fromDiffList :: DiffList a -> [a]
+fromDiffList (DiffList f) = f []
+
+instance Monoid (DiffList a) where
+    mempty = DiffList (\xs -> [] ++ xs)
+    (DiffList f) `mappend` (DiffList g) = DiffList (\xs -> f (g xs))
+
+instance Semigroup (DiffList a) where
+    (<>) = mappend
