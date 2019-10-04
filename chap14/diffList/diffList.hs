1ebab9876d9dc0e6d4e6de32fb65feb47af281c7
cipepser <respepic@gmail.com>
2019-10-05 08:25:27 +0900

    add finalCountDown

--- chap14/diffList/diffList.hs
+++ chap14/diffList/diffList.hs
@@ -26,3 +26,17 @@ gcdReverse a b
         result <- gcdReverse b (a `mod` b)
         tell (toDiffList [show a ++ " mod " ++ show b ++ " = " ++ show (a `mod` b)])
         return result
+
+finalCountDown :: Int -> Writer (DiffList String) ()
+finalCountDown 0 = do
+    tell (toDiffList ["0"])
+finalCountDown x = do
+    finalCountDown (x-1)
+    tell (toDiffList [show x])
+
+finalCountDown' :: Int -> Writer [String] ()
+finalCountDown' 0 = do
+    tell ["0"]
+finalCountDown' x = do
+    finalCountDown' (x-1)
+    tell [show x]
